61d6214e795925ef5af5db0f509aca80069237dd
puneetagarwal33 <puneet_kumar_agarwal@yahoo.co.in>
2025-03-08 09:28:26 +0530

    updated the refill logic to refresh after each second as per refresh rate

--- tokenbucket.py
+++ tokenbucket.py
@@ -1,32 +1,41 @@
 import time
-
 class TokenBucket:
     
-    def __init__(self, capacity, refill_rate_in_sec):
-        self.capacity = capacity
-        self.token = capacity
-        self.refill_rate_in_sec = refill_rate_in_sec
-        self.start_time = time.time()
+    def __init__(self, bucket_capacity, fill_rate_in_a_sec):
+        self.bucket_capacity = bucket_capacity
+        self.current_tokens = bucket_capacity
+        self.fill_rate_in_a_sec = fill_rate_in_a_sec
+        self.last_refill_time = time.time()
+        
     
     def refill(self):
         cur_time = time.time()
-        if cur_time - self.start_time > self.refill_rate_in_sec:
-            self.token = self.capacity
-            self.start_time = cur_time
-    
-    def allow(self) -> bool:
-        self.refill()
-        cur_time = time.time()
-        if self.token > 0:
-            self.token -= 1
-            return True
-        return False;
-    
-    
-tb = TokenBucket(10, 20)
-for i in range(1,50):
-        response = tb.allow();
-        print(f"response recieved {response}")
-        time.sleep(1)
+        time_diff = cur_time - self.last_refill_time
+        tokens_to_add = time_diff*self.fill_rate_in_a_sec;
         
+        if time_diff >= 1:
+            self.current_tokens = min(self.bucket_capacity, (self.current_tokens + tokens_to_add))
+            self.last_refill_time = time.time()
+    
+    def allow(self):
+        self.refill()
+        if self.current_tokens > 0:
+            self.current_tokens -= 1
+            return True;
+        return False;    
+    
+    
+token_bucket = TokenBucket(10, 10)
+for i in range(20):
+    for j in range(20):
+        response = token_bucket.allow()
+        print(f"request is allow ? {response}")
+    time.sleep(1)    
+    
+        
+
+    
+    
             
+        
+    
